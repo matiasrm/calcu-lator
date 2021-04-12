dc610589bb5ac00d7a7be83f79b13884f04b9e03
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-04-12 19:19:03 -0300

    Initial Commit

---
+++ calcu lator/calcu_latorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcu_latorApp.swift
+//  calcu lator
+//
+//  Created by Mati Montenegro on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct calcu_latorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
